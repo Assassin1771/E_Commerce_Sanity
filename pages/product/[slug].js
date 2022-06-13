327499094c181615ab29f1dbe76d8bc234165829
Tushar Garg <tshrgarg2010@gmail.com>
2022-06-13 19:47:00 +0530

    Adding context to different components

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -9,9 +9,14 @@ import {
   AiOutlineStar,
 } from "react-icons/ai";
 
+import { useStateContext } from "../../context/StateContext";
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+
+  const { decQty, incQty, qty } = useStateContext();
+
   return (
     <div>
       <div className="product-detail-container">
@@ -53,11 +58,11 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num">0</span>
-              <span className="plus" onClick="">
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
