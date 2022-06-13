35f9ff5fe594ba6704e1f48ab283a9c5d00473cc
Tushar Garg <tshrgarg2010@gmail.com>
2022-06-13 14:51:51 +0530

    Setting up index.js and CSS

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      Banner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers</p>
+      </div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      Footer
+    </>
+  );
 };
 
 export default Home;
